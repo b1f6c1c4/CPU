e581e632ab21f1d90f4bc319c1d923b9935e14cc
b1f6c1c4 <b1f6c1c4@gmail.com>
2016-09-18 19:03:26 +0800

    lpm_ram_256_8

--- Hardware/lpm_ram_256_8.v
+++ Hardware/lpm_ram_256_8.v
@@ -1,11 +1,21 @@
 `default_nettype none
 module lpm_ram_256_8(
    input Clock,
-   input Reset,
    input [7:0] address,
    input [7:0] data,
    input wren, // 1 is write; 0 is read
    output reg [7:0] q
 );
+
+   reg [7:0] ram[0:255];
+
+   always @(posedge Clock)
+      if (wren)
+         begin
+            q <= data;
+            ram[address] <= data;
+         end
+      else
+         q <= ram[address];
 
 endmodule
