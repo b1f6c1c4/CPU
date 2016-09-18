e9fba178ee9b219829da54189404df5bc40b4d0d
b1f6c1c4 <b1f6c1c4@gmail.com>
2016-09-18 19:32:04 +0800

    Flag

--- Hardware/Flag.v
+++ Hardware/Flag.v
@@ -3,7 +3,13 @@ module Flag(
    input Clock,
    input Reset,
    input [7:0] Flagin,
-   output [7:0] Flagout
+   output reg [7:0] Flagout
 );
+
+   always @(posedge Clock, negedge Reset)
+      if (~Reset)
+         Flagout <= 8'b0;
+      else
+         Flagout <= Flagin;
 
 endmodule
