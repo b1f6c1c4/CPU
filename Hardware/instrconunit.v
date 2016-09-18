5c93f303ce9a8f39c084a86fef8c2845665d281d
b1f6c1c4 <b1f6c1c4@gmail.com>
2016-09-18 19:54:11 +0800

    instrconunit

--- Hardware/instrconunit.v
+++ Hardware/instrconunit.v
@@ -5,7 +5,17 @@ module instrconunit(
    input BRANCH,
    input JUMP,
    input [7:0] imm,
-   output [7:0] PC
+   output reg [7:0] PC
 );
+
+   always @(posedge Clock, negedge Reset)
+      if (~Reset)
+         PC <= 8'b0;
+      else if (JUMP)
+         PC <= imm;
+      else if (BRANCH)
+         PC <= PC + imm + 8'b1;
+      else
+         PC <= PC + 8'b1;
 
 endmodule
