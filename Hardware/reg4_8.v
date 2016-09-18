4601be068d28c4b88e866acf1f6b11fb9f3fe1d4
b1f6c1c4 <b1f6c1c4@gmail.com>
2016-09-18 18:56:14 +0800

    reg4_8

--- Hardware/reg4_8.v
+++ Hardware/reg4_8.v
@@ -14,4 +14,26 @@ module reg4_8(
    input REG_WE // enable
 );
 
+   reg [7:0] registers[0:3];
+
+   // read channel 1
+   always @(*)
+      Q1 <= registers[N1];
+
+   // read channel 2
+   always @(*)
+      Q2 <= registers[N2];
+
+   // write channel
+   always @(posedge Clock, negedge Reset)
+      if (~Reset)
+         begin
+            registers[2'h0] = 8'b0;
+            registers[2'h1] = 8'b0;
+            registers[2'h2] = 8'b0;
+            registers[2'h3] = 8'b0;
+         end
+      else if (REG_WE)
+         registers[ND] = DI;
+
 endmodule
